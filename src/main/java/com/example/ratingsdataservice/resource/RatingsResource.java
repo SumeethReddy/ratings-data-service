699b3d8c5b681da4722e79db1cd4212af74c9497
Juvva <juvvaavi@ES.AD.ADP.com>
2021-12-16 15:00:15 -0500

    Initial commit

---
+++ src/main/java/com/example/ratingsdataservice/resource/RatingsResource.java
@@ -0,0 +1,29 @@
+package com.example.ratingsdataservice.resource;
+
+import com.example.ratingsdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3),
+                new Rating("234",5)
+        );
+        return ratings;
+    }
+}
